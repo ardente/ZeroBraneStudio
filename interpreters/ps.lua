c9428428c74983145e9fe83db8f8112905825b2f
ardente <vif.ardente@gmail.com>
2013-12-15 23:33:16 +0400

    pos interp name

--- interpreters/ps.lua
+++ interpreters/ps.lua
@@ -1,6 +1,4 @@
 dofile 'interpreters/luabase.lua'
-local interp = MakeLuaInterpreter(5.2, ' 5.2')
-interp.name = interp.name..' (ps)'
+local interp = MakeLuaInterpreter(5.2, ' 5.2 pos')
 interp.api = {"luabase", "pos_api"}
 return interp
-
